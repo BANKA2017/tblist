e1ff0106783bb2a12038c40d50c323adef172c6c
BANKA2017 <17263253+BANKA2017@users.noreply.github.com>
2023-02-18 11:24:50 +0800

    update: update row `updated_at` while run script `updateInfo.ts` update: use errmsg as real_fname while errno in [340000, 340001]

--- apps/crawlers/updateInfo.ts
+++ apps/crawlers/updateInfo.ts
@@ -13,7 +13,7 @@ let exist = sqlExist
 const total = sqlTotal
 
 
-while (tiebaList = db.query<[number, string, string]>("SELECT id, fname, gb2312_urlencode FROM tblite WHERE real_fname IS NULL LIMIT 50;").map(data => ({id: data[0], fname: data[1], gb2312_urlencode: data[2]}))) {
+while (tiebaList = db.query<[number, string, string]>("SELECT id, fname, gb2312_urlencode FROM tblite WHERE real_fname IS NULL LIMIT 60;").map(data => ({id: data[0], fname: data[1], gb2312_urlencode: data[2]}))) {
     if (tiebaList.length < 1) {
         console.log('tblist: ended')
         break
@@ -26,7 +26,7 @@ while (tiebaList = db.query<[number, string, string]>("SELECT id, fname, gb2312_
         db.transaction(() => {
             for (const tiebaInfo of tmpTiebaInfo) {
                 if (tiebaInfo.status === 'fulfilled' && tiebaInfo.value?.response && !tiebaInfo.value?.response?.errno) {
-                    db.query("UPDATE tblite SET real_fname = :real_fname, fid = :fid, member_num = :member_num, post_num = :post_num, thread_num = :thread_num WHERE id = :id", {
+                    db.query("UPDATE tblite SET real_fname = :real_fname, fid = :fid, member_num = :member_num, post_num = :post_num, thread_num = :thread_num, updated_at = datetime('now') WHERE id = :id", {
                         id: tiebaInfo?.value?.fname?.id || tiebaInfo?.reason?.fname?.id,
                         real_fname: tiebaInfo.value.response?.data?.forum?.name || '',
                         fid: tiebaInfo.value.response?.data?.forum?.id || 0,
@@ -34,10 +34,10 @@ while (tiebaList = db.query<[number, string, string]>("SELECT id, fname, gb2312_
                         post_num: tiebaInfo.value.response?.data?.forum?.post_num || 0,
                         thread_num: tiebaInfo.value.response?.data?.forum?.thread_num || 0
                     })
-                } else if (tiebaInfo.status === 'fulfilled' && tiebaInfo.value?.response && tiebaInfo.value?.response?.errno === 340001) {
-                    db.query("UPDATE tblite SET real_fname = :real_fname, fid = :fid, member_num = :member_num, post_num = :post_num, thread_num = :thread_num WHERE id = :id", {
+                } else if (tiebaInfo.status === 'fulfilled' && tiebaInfo.value?.response && [340000, 340001].includes(tiebaInfo.value?.response?.errno)) {
+                    db.query("UPDATE tblite SET real_fname = :real_fname, fid = :fid, member_num = :member_num, post_num = :post_num, thread_num = :thread_num, updated_at = datetime('now') WHERE id = :id", {
                         id: tiebaInfo?.value?.fname?.id || tiebaInfo?.reason?.fname?.id,
-                        real_fname: '抱歉，根据相关法律法规和政策，本吧暂不开放，去看看其他贴吧',//tiebaInfo.value.response?.data?.forum?.name || '',
+                        real_fname: tiebaInfo.value?.response?.errmsg,//tiebaInfo.value.response?.data?.forum?.name || '',
                         fid: tiebaInfo.value.response?.data?.forum?.id || 0,
                         member_num: tiebaInfo.value.response?.data?.forum?.member_num || 0,
                         post_num: tiebaInfo.value.response?.data?.forum?.post_num || 0,
